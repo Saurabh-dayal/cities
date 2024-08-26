c54ea2302c90f813ab4625c201e427e9991f73fb
Saurabh-dayal <saurabhdayalsolanki@gmail.com>
2024-08-26 23:40:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Tours from './Components/Tours';
+import Data from './Components/Data';
 
 function App() {
+  const [tours, setTours] = useState(Data);
+
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(tours.length ===0){
+    return( 
+      <div className='refresh'>
+        <h2>No Tools Left</h2>
+        <button className='btn-white' onClick={() => setTours(Data)}> Refresh</button>
+      </div>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tours tours={tours} removeTour={removeTour} />
     </div>
   );
 }
---
+++ src/Components/Data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+      id: 1,
+      name: "Agra",
+      info: "Agra, located in the northern Indian state of Uttar Pradesh, is a city steeped in history and culture, renowned for its Mughal-era architecture. It is home to the iconic Taj Mahal, one of the Seven Wonders of the World and a UNESCO World Heritage Site, celebrated for its stunning white marble beauty and romantic history. Agra also boasts other historical landmarks such as the Agra Fort and Fatehpur Sikri, showcasing the grandeur of Mughal architecture and artistry. The city's rich heritage, vibrant markets, and delectable cuisine, including the famous Petha sweet, make it a captivating destination for tourists and history enthusiasts alike.",
+      image: "/Images/agra.jpg",
+      price: "35,758",
+    },
+    {
+      id: 2,
+      name: "Jaipur",
+      info: "Jaipur, the vibrant capital of Rajasthan in northern India, is famously known as the Pink City due to its distinctive pink-colored buildings. Founded in 1727 by Maharaja Sawai Jai Singh II, Jaipur is a city rich in history and culture, featuring a blend of ancient forts, palaces, and temples. Key attractions include the stunning Hawa Mahal (Palace of Winds), the majestic Amer Fort, and the City Palace, which offers a glimpse into the royal lifestyle of Jaipur's past rulers. The city's well-planned layout, bustling bazaars filled with handicrafts and jewelry, and the annual Jaipur Literature Festival further enhance its appeal, making Jaipur a must-visit destination for travelers seeking a mix of heritage, culture, and modernity.",
+      image: "/Images/jaipur.jpg",
+      price: "85,708",
+    },
+    {
+      id: 3,
+      name: "Goa",
+      info: "Goa, India's smallest state by area, is a coastal paradise known for its stunning beaches, vibrant nightlife, and Portuguese heritage. Located on the western coast along the Arabian Sea, Goa attracts tourists with its diverse attractions, from the serene and scenic beaches like Baga, Anjuna, and Palolem to the bustling markets of Mapusa and Panaji. The state's rich history is reflected in its colonial architecture, with landmarks such as the Basilica of Bom Jesus and Se Cathedral standing as testaments to its Portuguese past. Goa's unique blend of Indian and Portuguese cultures is evident in its cuisine, music, and festivals, making it a lively and diverse destination that offers a perfect mix of relaxation, adventure, and cultural exploration.",
+      image: "/Images/goa.jpg",
+      price: "29,008",
+    },
+    {
+      id: 4,
+      name: "Varanasi",
+      info: "Varanasi, also known as Kashi or Benares, is one of the oldest continuously inhabited cities in the world and a major spiritual hub in India. Situated on the banks of the sacred Ganges River in Uttar Pradesh, Varanasi is revered in Hinduism, Jainism, and Buddhism. The city is renowned for its ghats—stepped banks leading to the river—where pilgrims perform ritual ablutions and ceremonies. Notable ghats include Dashashwamedh, Assi, and Manikarnika. Varanasi is also famous for its vibrant cultural scene, classical music, silk weaving, and ancient temples, with the Kashi Vishwanath Temple being the most significant. The city's labyrinthine streets, bustling markets, and the serene Ganga Aarti ceremony create a unique, timeless atmosphere that attracts pilgrims, tourists, and seekers of spirituality from around the world.",
+      image: "/Images/varanasi.jpg",
+      price: "31,098",
+    },
+    {
+      id: 5,
+      name: "Darjeeling",
+      info: "Darjeeling, nestled in the foothills of the eastern Himalayas in West Bengal, India, is renowned for its stunning natural beauty, tea plantations, and colonial charm. Often referred to as the Queen of the Hills, Darjeeling offers breathtaking views of snow-capped peaks, including the majestic Kanchenjunga, the world's third-highest mountain. The city's lush tea gardens produce the globally famous Darjeeling tea, prized for its unique flavor. Attractions such as the Darjeeling Himalayan Railway, a UNESCO World Heritage site, and the serene Peace Pagoda add to the city's allure. The cool climate, vibrant local culture, bustling markets, and historic schools make Darjeeling a popular destination for both leisure and education, drawing visitors from across the globe to experience its tranquil and picturesque landscape.",
+      image: "/Images/darjeeling.jpg",
+      price: "95,098",
+    },
+    {
+      id: 6,
+      name: "Jaisalmer",
+      info: "Jaisalmer, known as the Golden City, is a captivating desert town in the Indian state of Rajasthan, famed for its yellow sandstone architecture that glows golden under the sun. Dominated by the imposing Jaisalmer Fort, a UNESCO World Heritage site, this living fort houses palaces, temples, and havelis, showcasing intricate carvings and a rich history. The city's narrow streets are lined with bustling bazaars, offering traditional Rajasthani handicrafts, textiles, and jewelry. Nearby, the vast Thar Desert beckons with its sweeping dunes, offering unique experiences like camel safaris and desert camping under star-lit skies. Jaisalmer's vibrant culture, historic charm, and desert adventures make it a mesmerizing destination for travelers seeking a blend of history, tradition, and natural beauty.",
+      image: "/Images/jaisalmer.jpg",
+      price: "80,058",
+    },
+    {
+      id: 7,
+      name: "Kochi",
+      info: "Kochi, also known as Cochin, is a vibrant port city on the southwest coast of India in the state of Kerala. Known as the Queen of the Arabian Sea, Kochi has a rich maritime history and has been a significant trade center for centuries. The city's diverse cultural heritage is reflected in its colonial architecture, ancient mosques, Jewish synagogues, and the charming Chinese fishing nets that line its shores. Key attractions include the historic Fort Kochi area, the Mattancherry Palace, and the Paradesi Synagogue. Kochi is also a gateway to Kerala's scenic backwaters and lush landscapes. With its blend of traditional Kerala culture, modern urban life, and a cosmopolitan vibe, Kochi offers a unique and dynamic experience for visitors.",
+      image: "/Images/kochi.jpg",
+      price: "100,058",
+    },
+  ];
+  
+  export default data;
+  
